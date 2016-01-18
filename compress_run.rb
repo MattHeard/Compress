b40f506a47e40cfd4cf7e9f2e258e17397a6e9a1
Lena Plaksina <lena.plaksina@gmail.com>
2016-01-18 16:02:53 +1300

    Compress by building regex

--- compress_run.rb
+++ compress_run.rb
@@ -1,5 +1,6 @@
 require_relative 'compressor'
 
-compressed = Compressor.new('And then it happened; I think it\'s over now.').compress
+# compressed = Compressor.new("The quick brown fox jumps over the lazy dog").compress
+compressed = Compressor.new(File.read("sample.txt")).compress
 
 Compressor.new(compressed).decompress
--- compressor.rb
+++ compressor.rb
@@ -1,35 +1,61 @@
-require_relative 'ruleset'
+require_relative 'ruleset_symbols_to_esc'
+require_relative 'ruleset_word_to_symbol'
 
 class Compressor
   def initialize(text)
     @text = text
-    @ruleset = RULESET
+    @words_ruleset = WORDS_RULESET
+    @symbols_ruleset = SYMBOLS_RULESET
   end
 
-  def substitute(ruleset)
-    ruleset.each do |rule|
-      apply_rule(rule)
+  # def substitute(ruleset)
+  #   ruleset.each do |rule|
+  #     apply_rule(rule)
+  #   end
+  #   @text
+  # end
+  #
+  # def apply_rule(rule)
+  #   # @text.gsub!(/[^1]#{rule[0]}/, rule[1])
+  #   # @text = @text.split(rule[0]).join(rule[1])
+  #
+  # end
+
+  def substitute(ruleset, comp_type)
+    symbol_table = Hash[ruleset]  #  { abbr: full_text, ... }
+
+    symbol_regexp = Regexp.new("(" + symbol_table.keys.map { |k| Regexp.escape(k) }.join("|") + ")")
+    @text.gsub(symbol_regexp) { |abbr| symbol_table[abbr] }.tap do |result|
+      File.write('output/' + comp_type + '.txt', result)
     end
-    @text
-  end
-
-  def apply_rule(rule)
-    @text.gsub!(/#{rule[0]}/, rule[1])
   end
 
   def compress
-    compressed = substitute(@ruleset)
-    File.write('output/compressed.txt', compressed)
-    compressed
+    ruleset = @symbols_ruleset + @words_ruleset
+
+    # ruleset.each do |rule|
+    #   @text.gsub!(/(?=[^1])(#{rule[0]})/,rule[1])
+    # end
+    # File.write('output/compressed.txt', @text)
+    # @text
+
+    substitute(ruleset, 'compressed')
   end
 
   def decompress
-    swapped_rules = swap_rule_pairs(@ruleset)
-    new_rules = reverse_rule_order(swapped_rules)
+    reversed_symbols_rules = reverse_rule_order(@symbols_ruleset)
+    reversed_words_rules = reverse_rule_order(@words_ruleset)
+    swapped_rules = swap_rule_pairs(reversed_symbols_rules + reversed_words_rules)
 
-    decompressed = substitute(new_rules)
-    File.write('output/decompressed.txt', decompressed)
-    decompressed
+    # swapped_rules :: [[abbr, full_text]]
+    # symbol_table = Hash[swapped_rules]  #  { abbr: full_text, ... }
+    #
+    # symbol_regexp = Regexp.new("(" + symbol_table.keys.map { |k| Regexp.escape(k) }.join("|") + ")")
+    # @text.gsub(symbol_regexp) { |abbr| symbol_table[abbr] }.tap do |decompressed|
+    #   File.write('output/decompressed.txt', decompressed)
+    # end
+
+    substitute(swapped_rules, 'decompressed')
   end
 
   def swap_rule_pairs(ruleset)
